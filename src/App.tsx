17c8c965365d23a483b4171ee0f6a49f6c26787d
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2022-09-04 00:12:26 +0900

    add DnD

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ function App() {
   const [selectValue, setSelectValue] = useState(0)
   const [columns, setColumns] = useState([[{ fromId: 11, toId: 1 }]])
   const [contentColumns, setContentColumns] = useState([[0]])
-  const [edges, setEdges] = useState<any>([{}])
+  const [edges, setEdges] = useState([{}])
+  const [selectedNode, setSelectedNode] = useState(0)
+  const [template, setTemplate] = useState([[0]])
 
   const fetchGraph = async () =>{
     let response = await fetch(`/api/graphs/${selectValue}`)
@@ -79,6 +81,62 @@ function App() {
     setEdges(createEdges({requestedGraph,contentColumns}))
   },[contentColumns])
 
+  const dragStart =(id:number)=>{
+    setSelectedNode(id)
+  }
+  const dragEnd =()=>{
+    setSelectedNode(0)
+    setContentColumns(template)
+  }
+
+  const dragOver = (evt:any) => {
+    // Разрешаем сбрасывать элементы в эту область
+    evt.preventDefault();
+ 
+    const activeElement = selectedNode
+    const Columns = [...contentColumns]
+    // Находим элемент, над которым в данный момент находится курсор
+    const currentElement = parseInt(evt.target.getAttribute('id'))
+    // Проверяем, что событие сработало:
+    // 1. не на том элементе, который мы перемещаем,
+    // 2. именно на элементе списка
+    const isMoveable = activeElement !== currentElement
+    // Если нет, прерываем выполнение функции
+
+    let currentid = 0 
+    let currentcol = 0
+    let deleteEl = 0
+    if (!!isMoveable) {
+      for (let i = 0; i < contentColumns.length; i++){
+        for (let j = 0; j < contentColumns[i].length; j++){
+          if(currentElement === contentColumns[i][j]){
+            if(contentColumns[i].some((i)=>i === activeElement)){
+              if (j < contentColumns[i].indexOf(activeElement)){
+                currentid = j
+                currentcol = i
+                deleteEl = contentColumns[i].indexOf(activeElement) + 1
+              }else{
+                currentid = j + 1
+                currentcol = i
+                deleteEl = contentColumns[i].indexOf(activeElement)
+              }
+            }
+
+          }
+        }
+      }
+    }
+
+    if (!!isMoveable) {
+      let arr = [...Columns[currentcol]]
+      arr.splice(currentid, 0 , activeElement)
+      arr.splice(deleteEl, 1)
+      Columns[currentcol] = arr
+    }
+    setTemplate(Columns)
+  }
+
+
   return(
     <div className="wrapper">
       <div>
@@ -102,10 +160,15 @@ function App() {
             <div
              className="node" 
              key={nodeIndex} 
+             draggable="true"
+             onDragStart={()=>dragStart(node)}
+             onDragEnd={()=>dragEnd()}
+             onDragOver={dragOver}
+             id={String(node)}
              >
             {requestedGraph.nodes.filter(
               (i) => i.id === node).map(
-              (j) => {return <div>{j.name}</div>}
+              (j) => {return <div id={String(node)} >{j.name}</div>}
               )}</div>
               </div>})}
               </div>})
